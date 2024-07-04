e7280ab23b7718b2b7502f6f90c462b3330b5342
torihazi <lovebarusa19981225@icloud.com>
2024-07-04 20:27:33 +0900

    fin chapter 32

--- src/components/task/TaskCard.jsx
+++ src/components/task/TaskCard.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import TaskCardTitle from "./TaskCardTitle";
 import TaskCardDeleteButton from "./button/TaskCardDeleteButton";
 import TaskAddInput from "./input/TaskAddInput";
 import Tasks from "./Tasks";
 
 const TaskCard = () => {
+  const [inputText, setInputText] = useState("");
+  const [taskList, setTaskList] = useState([]);
   return (
     <div className="taskCard">
       <TaskCardTitle />
       <TaskCardDeleteButton />
-      <TaskAddInput />
+      <TaskAddInput
+        inputText={inputText}
+        setInputText={setInputText}
+        taskList={taskList}
+        setTaskList={setTaskList}
+      />
       <Tasks />
     </div>
   );
--- src/components/task/input/TaskAddInput.jsx
+++ src/components/task/input/TaskAddInput.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 
-const TaskAddInput = () => {
+const TaskAddInput = (props) => {
+  const { inputText, setInputText, taskList, setTaskList } = props;
   const handleSubmit = (e) => {
     e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    setInputText(e.target.value);
   };
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="add a task" className="taskAddInput" />
+        <input
+          type="text"
+          placeholder="add a task"
+          className="taskAddInput"
+          onChange={handleChange}
+        />
       </form>
     </div>
   );
